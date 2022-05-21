79469b6b9b73446d0d6f16f940e2f26f6abb570b
JessePullen <jessepullen@icloud.com>
2022-05-21 20:58:05 +1000

    Finish task 3 JS exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let wordArray = []
+    for (let i = 0; i < word.length; i++)  {
+        wordArray.push(word[i]);
+    }
+    wordArray.reverse();
+    return wordArray.join('');
 };
 
 // Do not edit below this line
