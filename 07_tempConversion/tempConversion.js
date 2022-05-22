0cee550dce443d16958c9c9712a6ea09d7d23bf5
JessePullen <jessepullen@icloud.com>
2022-05-22 14:01:44 +1000

    Finish task 7 JS exercises

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,16 @@
-const ftoc = function() {
-
+const ftoc = function(fTemp) {
+  let temp = (fTemp - 32) * 5/9;
+  return round(temp);
 };
 
-const ctof = function() {
-
+const ctof = function(cTemp) {
+  let temp = (cTemp * 9/5) + 32
+  return round(temp);
 };
+
+const round = function(num) {
+  return Math.round(num * 10) / 10;
+}
 
 // Do not edit below this line
 module.exports = {
