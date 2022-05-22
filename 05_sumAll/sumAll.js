918cf3f1adce924217e5295be8d0a3a029903c4a
JessePullen <jessepullen@icloud.com>
2022-05-22 11:27:01 +1000

    Finish task 5 JS exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    
+    if (num1 < 0 || num2 < 0 || !Number.isInteger(num1) || !Number.isInteger(num2)) {
+        return 'ERROR';
+    }
 
+    if (num1 > num2) {
+        endingNum = num1;
+        startingNum = num2
+    } else  {
+        startingNum = num1;
+        endingNum = num2;
+    }
+
+    for (startingNum; startingNum <= endingNum; startingNum++) {
+        sum += startingNum;    
+    }
+    return sum;
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
