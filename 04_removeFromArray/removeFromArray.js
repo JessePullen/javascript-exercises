37abf767e4d390c13cdecd608516b1dc1cb42cf6
JessePullen <jessepullen@icloud.com>
2022-05-22 11:03:26 +1000

    Finish task 4 JS exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...items) {
+    for (item of items) {
+        let index = array.indexOf(item);
+        if (array.includes(item)) {
+            array.splice(index, 1);
+        } else {
+            continue
+        }
+    } 
+    return array;
 };
 
 // Do not edit below this line
