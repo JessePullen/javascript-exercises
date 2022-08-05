8bbc2d29cf8a2ab42a4339a44c2658260f4ae709
JessePullen <jessepullen@icloud.com>
2022-08-05 13:45:15 +1000

    Complete task 8 calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,52 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  total = 0;
+
+  let aLen = array.length;
+
+  for (let i  = 0;  i < aLen; i++) {
+    total += array[i];
+  }
+
+  return total;
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  let aLen = array.length;
 
+  total = array[0];
+
+  for (let i  = 1;  i < aLen; i++) {
+    total *= array[i];
+  }
+
+  return total;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0 || num === 1) {
+    return 1;
+  }
+
+  let total = num;
+
+  for (i = num - 1; i > 0; i--) {
+    total *= i;
+    num--
+  }
+
+  return total;
 };
 
 // Do not edit below this line
