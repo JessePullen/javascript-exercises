863827a79bec304bcbf4b850fdc150204f0fb830
JessePullen <jessepullen@icloud.com>
2022-08-26 12:48:29 +1000

    Finish task 9 JS exercises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,21 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    newStr = str.toLowerCase().replace(/[^\w\s\']|_/g, "").replace(/\s+/g, "");
 
+    if (newStr === reverseWord(newStr)) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
+function reverseWord(str) {
+    let word = str.split("");
+
+    let reverseWord = word.reverse();
+
+    let joinWord = reverseWord.join("");
+
+    return joinWord;
+}
 // Do not edit below this line
 module.exports = palindromes;
