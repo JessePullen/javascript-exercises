480575d3c6e81d50b4bb4e21a094ea3b72bd0598
JessePullen <jessepullen@icloud.com>
2022-08-27 20:08:08 +1000

    Finish task 10 JS exercises

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    console.log('test');
 
-};
+    i = 0;
+    newArray = [];
+
+    while (i < num) {
+        if (newArray.length < 2) {
+            j = 1;
+        } else {
+            j = newArray[i-1] + newArray[i-2];
+        }
+        
+        newArray.push(j);
+        i++
+    }
+
+    if (num >= 0) {
+        return newArray[num-1];
+    } else {
+        return 'OOPS'; 
+    }
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
