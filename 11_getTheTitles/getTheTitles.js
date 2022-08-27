a4b2f876146cd27be0298a8c534a1fd4f710ba44
JessePullen <jessepullen@icloud.com>
2022-08-27 22:37:49 +1000

    Finish task 11 JS exercises

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let bookTitles = books.map((books) => books.title)
+    return bookTitles;
 };
 
 // Do not edit below this line
