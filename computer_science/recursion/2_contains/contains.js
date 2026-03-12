64a1d14aaf03d21fd39d40c09a5fd3aac870b4fa
Jesse <jessepullen@icloud.com>
2026-03-12 12:59:35 +1000

    Finish problem 2 of recursion

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,6 +1,22 @@
-const contains = function() {
-  
+const contains = function (list, word) {
+  //base case
+  if (list === word) {
+    return true;
+  }
+
+  // recursive logic
+  if (list && typeof list === "object") {
+    for (const value of Object.values(list)) {
+      if (value === word) {
+        return true;
+      }
+      if (contains(value, word)) {
+        return true;
+      }
+    }
+  }
+  return false;
 };
-  
+
 // Do not edit below this line
 module.exports = contains;
