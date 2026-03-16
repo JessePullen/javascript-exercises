d0fb58236e24c6a00ea7aad66a4057a4bb3d725b
Jesse <jessepullen@icloud.com>
2026-03-16 15:35:03 +1000

    Finish and test problem 3

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,6 +1,20 @@
-const totalIntegers = function() {
-  
+const totalIntegers = function (list) {
+  let total = 0;
+
+  const newList = Object.values(list);
+
+  for (const value of newList) {
+    if (typeof value === "number") {
+      total++;
+    }
+
+    if (typeof value === "object") {
+      total += totalIntegers(value);
+    }
+  }
+
+  return total;
 };
-  
+
 // Do not edit below this line
 module.exports = totalIntegers;
