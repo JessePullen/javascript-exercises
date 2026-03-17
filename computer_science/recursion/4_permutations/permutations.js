4263db371535a1ab6284eeb60fe17de02e461641
Jesse <jessepullen@icloud.com>
2026-03-17 13:35:27 +1000

    Finish and test problem 4

--- computer_science/recursion/4_permutations/permutations.js
+++ computer_science/recursion/4_permutations/permutations.js
@@ -1,6 +1,21 @@
-const permutations = function() {
-  
-};
+const permutations = function (nums) {
+  if (nums.length === 0) {
+    return [nums];
+  }
+
+  let result = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    let current = nums[i]
+    let remaining = nums.slice(0, i).concat(nums.slice(i + 1))
+    let subPermutation = permutations(remaining)
+
+    for (const num of subPermutation) {
+      result.push([current, ...num])
+    }
+  }
+  return result
+}
   
 // Do not edit below this line
 module.exports = permutations;
