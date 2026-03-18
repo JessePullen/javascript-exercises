98cfc2946d9f1278e42a99cc35c039afd977c539
Jesse <jessepullen@icloud.com>
2026-03-18 12:26:01 +1000

    Finish and test problem 5

--- computer_science/recursion/5_pascal/pascal.js
+++ computer_science/recursion/5_pascal/pascal.js
@@ -1,6 +1,24 @@
-const pascal = function() {
-  
+const pascal = function (n) {
+  if (n <= 0) {
+    return [];
+  }
+  // base case
+  if (n === 1) {
+    return [1];
+  }
+
+  let result = [];
+  let prev = pascal(n - 1);
+
+  result.push(1);
+
+  for (let i = 0; i < prev.length - 1; i++) {
+    result.push(prev[i] + prev[i + 1]);
+  }
+
+  result.push(1);
+  return result;
 };
-  
+
 // Do not edit below this line
 module.exports = pascal;
